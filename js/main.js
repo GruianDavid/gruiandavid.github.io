30cd96fae3ac9a2933b7e9d4c488468977470a0d
David Gruian <david.gruian@gmail.com>
2020-10-29 02:32:39 +0200

    added nanoscroll

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 jQuery(document).ready(function($){
+
+    //nano scroll init
+    $(".nano").nanoScroller();
+
+    //for smooth scroll on navigation click
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
-
+        //set active menu
         $('.nav-element a').removeClass('active');
         $(this).addClass('active');
+
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
