d6e6ddb0b12f96fd8d5dadfd1c806748ef993dff
David Gruian <david.gruian@gmail.com>
2020-10-29 03:44:18 +0200

    fixed navigation, added scroll spy and nav color change on scroll

--- js/main.js
+++ js/main.js
@@ -10,8 +10,28 @@ jQuery(document).ready(function($){
         $('.nav-element a').removeClass('active');
         $(this).addClass('active');
 
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
+        $(".nano").nanoScroller({ scrollTo: $.attr(this, 'href') });
     });
+
+    $(".nano-content").on('scroll', function() {
+        let distanceFromTop = $(".nano-content").scrollTop(); 
+        if (distanceFromTop > 50){
+            $('.nav').addClass('filled');
+        }else{
+            $('.nav').removeClass('filled');
+        }
+        var currentTop = $(window).scrollTop();
+        var elems = $('.section');
+        elems.each(function(index){
+          var elemTop 	= $(this).offset().top;
+          var elemBottom 	= elemTop + $(this).height();
+          if(currentTop >= elemTop && currentTop <= elemBottom){
+            var id 		= $(this).attr('id');
+            var navElem = $('a[href="#' + id+ '"]');
+            $('.nav-element a').removeClass('active');
+            navElem.addClass('active')
+          }
+        })
+    }); 
+
 });
